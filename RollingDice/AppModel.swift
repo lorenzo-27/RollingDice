5e63dfaf350ce5c1e41108a4ca91c6a6429680fd
Lorenzo Benedetti <lorenzo.benedetti5@edu.unifi.it>
2025-04-21 12:20:00 +0200

    Initial Commit

---
+++ RollingDice/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  RollingDice
+//
+//  Created by Lorenzo Benedetti on 21/04/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ RollingDice/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  RollingDice
+//
+//  Created by Lorenzo Benedetti on 21/04/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
